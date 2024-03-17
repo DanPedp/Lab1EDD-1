f6578f244a4d87e147f6823d772b980ba7c37e7f
El Zapato <163614217+DanPedp@users.noreply.github.com>
2024-03-17 17:27:33 -0500

    AAAAAA
    AAAAAAAA

--- EDDlab1/src/eddlab1/EDDlab1.java
+++ EDDlab1/src/eddlab1/EDDlab1.java
@@ -1,7 +1,11 @@
 package eddlab1;
+
 public class EDDlab1 {
+
     public static void main(String[] args) {
         System.out.println("Hola soy santiago");
+        System.out.println("aaaaaaaa");
+        System.out.println("___________________");
     }
-    
+
 }
